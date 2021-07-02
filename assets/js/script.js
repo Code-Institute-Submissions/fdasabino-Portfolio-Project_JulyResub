494f07ae4c225e99faf47704b6e4232660a72fc4
Francisco Sabino <fdasabino@gmail.com>
2021-07-02 12:03:24 +0200

    Adjust JS functionsn

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,80 +32,81 @@ closeBtns.forEach((node) => {
 	});
 });
 
-// Send email form
-//Sendmail functions
-function sendmail(name, email, msg) {
-	emailjs.send("service_6pcnbro", "template_hbodrvd", {
-		from_name: name,
-		from_email: email,
-		message: msg,
-	});
-}
-
-function emptyerror() {
-	Swal.fire({
-		icon: "info",
-		iconColor: "#D4873D",
-		title: "No Empty Forms Allowed",
-		text: "Please fill all the Fields",
-		confirmButtonColor: "#D4873D",
-		background: "#111",
-		closeOnClickOutside: true,
-	});
-}
-
-function weirdcharerror() {
-	Swal.fire({
-		icon: "info",
-		iconColor: "#D4873D",
-		title: "Only Letters Allowed",
-		text: "No Numbers or Special Characters",
-		confirmButtonColor: "#D4873D",
-		background: "#111",
-		closeOnClickOutside: true,
-	});
-}
-
-function errorname() {
-	Swal.fire({
-		icon: "question",
-		iconColor: "#D4873D",
-		title: "Have you Filled Your Name Correctly? ",
-		text: "Please fill Name Correctly ",
-		confirmButtonColor: "#D4873D",
-		background: "#111",
-		closeOnClickOutside: true,
-	});
-}
-
-function errormail() {
-	Swal.fire({
-		icon: "question",
-		iconColor: "#D4873D",
-		title: "Have you Filled Your Email Correctly? ",
-		text: "youremail@example.com",
-		confirmButtonColor: "#D4873D",
-		background: "#111",
-		closeOnClickOutside: true,
-	});
-}
-
-function success() {
-	Swal.fire({
-		icon: "success",
-		title: "Success",
-		text: "Your Message has Been Sent",
-		confirmButtonColor: "#D4873D",
-		background: "#111",
-		closeOnClickOutside: true,
-	});
-}
+// Validation function
 
 function validate() {
 	let name = document.getElementById("username");
 	let email = document.getElementById("email");
 	let msg = document.getElementById("message");
 	let btn = document.querySelector(".submit");
+
+	//Sendmail function
+	function sendmail(name, email, msg) {
+		emailjs.send("service_6pcnbro", "template_hbodrvd", {
+			from_name: name,
+			from_email: email,
+			message: msg,
+		});
+	}
+
+	function emptyerror() {
+		Swal.fire({
+			icon: "info",
+			iconColor: "#D4873D",
+			title: "No Empty Forms Allowed",
+			text: "Please fill all the Fields",
+			confirmButtonColor: "#D4873D",
+			background: "#111",
+			closeOnClickOutside: true,
+		});
+	}
+
+	function weirdcharerror() {
+		Swal.fire({
+			icon: "info",
+			iconColor: "#D4873D",
+			title: "Only Letters Allowed",
+			text: "No Numbers or Special Characters",
+			confirmButtonColor: "#D4873D",
+			background: "#111",
+			closeOnClickOutside: true,
+		});
+	}
+
+	function errorname() {
+		Swal.fire({
+			icon: "question",
+			iconColor: "#D4873D",
+			title: "Have you Filled Your Name Correctly? ",
+			text: "Please fill Name Correctly ",
+			confirmButtonColor: "#D4873D",
+			background: "#111",
+			closeOnClickOutside: true,
+		});
+	}
+
+	function errormail() {
+		Swal.fire({
+			icon: "question",
+			iconColor: "#D4873D",
+			title: "Have you Filled Your Email Correctly? ",
+			text: "youremail@example.com",
+			confirmButtonColor: "#D4873D",
+			background: "#111",
+			closeOnClickOutside: true,
+		});
+	}
+
+	function success() {
+		Swal.fire({
+			icon: "success",
+			title: "Success",
+			text: "Your Message has Been Sent",
+			confirmButtonColor: "#D4873D",
+			background: "#111",
+			closeOnClickOutside: true,
+		});
+	}
 
 	btn.addEventListener("click", (e) => {
 		e.preventDefault();
@@ -133,40 +134,39 @@ function validate() {
 			}, 7000);
 		}
 	});
-}
-
-// no number input for name field
-document
-	.getElementById("username")
-	.addEventListener("keydown", function (event) {
-		if (event.ctrlKey || event.altKey) {
-			event.preventDefault();
-		} else {
-			var key = event.keyCode;
-			if (
-				!(
-					key === 8 || // backspace
-					key === 9 || // tab
-					key === 13 || // enter
-					key === 20 || // caps lock
-					key === 16 || // shift - left and right
-					key === 32 || // space-bar
-					key === 27 || // escape
-					key === 46 || // delete
-					key === 116 || // f5
-					key === 221 || // å
-					key === 222 || // ä
-					key === 192 || // ö
-					key === 191 || // '
-					key === 189 || // -
-					(key >= 35 && key <= 40) ||
-					(key >= 65 && key <= 90)
-				)
-			) {
+	// no number input for name field
+	document
+		.getElementById("username")
+		.addEventListener("keydown", function (event) {
+			if (event.ctrlKey || event.altKey) {
 				event.preventDefault();
-				weirdcharerror();
+			} else {
+				var key = event.keyCode;
+				if (
+					!(
+						key === 8 || // backspace
+						key === 9 || // tab
+						key === 13 || // enter
+						key === 20 || // caps lock
+						key === 16 || // shift - left and right
+						key === 32 || // space-bar
+						key === 27 || // escape
+						key === 46 || // delete
+						key === 116 || // f5
+						key === 221 || // å
+						key === 222 || // ä
+						key === 192 || // ö
+						key === 191 || // '
+						key === 189 || // -
+						(key >= 35 && key <= 40) ||
+						(key >= 65 && key <= 90)
+					)
+				) {
+					event.preventDefault();
+					weirdcharerror();
+				}
 			}
-		}
-	});
+		});
+}
 
 validate();
